f13a71cdff237cbe5e46461d502ce064e3f93f51
ﾀｯｻﾝ <kndf_tassan0823@ezweb.ne.jp>
2017-10-20 17:06:53 +0900

    Database Adapter Createad

--- app/src/main/java/com/muroigundan/task_app/MainActivity.java
+++ app/src/main/java/com/muroigundan/task_app/MainActivity.java
@@ -2,11 +2,14 @@ package com.muroigundan.task_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
     private Realm mRealm;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRealm = Realm.getDefaultInstance();
+
+        mListView = (ListView) findViewById(R.id.listView);
+        RealmResults<Task> tasks = mRealm.where(Task.class).findAll();
+        SchedulerAdapter adapter = new SchedulerAdapter(tasks);
+        mListView.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/muroigundan/task_app/SchedulerAdapter.java
+++ app/src/main/java/com/muroigundan/task_app/SchedulerAdapter.java
@@ -1,5 +1,6 @@
 package com.muroigundan.task_app;
 
+import android.icu.text.SimpleDateFormat;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,15 +24,24 @@ public class SchedulerAdapter extends RealmBaseAdapter<Task> {
     }
 
     @Override
-    public View getView(int i, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_2, parent, false);
             viewHolder = new ViewHolder();
-            viewHolder.subject= (TextView) convertView.findViewById(R.id.text1);
-
+            viewHolder.subject= (TextView) convertView.findViewById(android.R.id.text1);
+            viewHolder.deadline= (TextView) convertView.findViewById(android.R.id.text2);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder)convertView.getTag();
         }
+
+        Task task = adapterData.get(position);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/HH/mm");
+        String formatTask = sdf.format(task.getDeadline());
+        viewHolder.subject.setText(task.getSubject());
+        viewHolder.deadline.setText(formatTask);
         return null;
     }
 

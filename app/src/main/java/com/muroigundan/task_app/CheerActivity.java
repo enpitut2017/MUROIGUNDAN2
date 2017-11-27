85951c03b3912d5cc068f8c86553dcb0ffd60259
ﾀｯｻﾝ <kndf_tassan0823@ezweb.ne.jp>
2017-11-27 14:01:25 +0900

    状態遷移の作成

--- app/src/main/java/com/muroigundan/task_app/CheerActivity.java
+++ app/src/main/java/com/muroigundan/task_app/CheerActivity.java
@@ -2,16 +2,21 @@ package com.muroigundan.task_app;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.Random;
 
 public class CheerActivity extends AppCompatActivity {
     long taskId;
+    int state = 1;
+    ImageView imageView = (ImageView)findViewById(R.id.imageView);
+    Button trans1 = (Button)findViewById(R.id.trans1);
+    Button trans2 = (Button)findViewById(R.id.trans2);
 
     AlertDialog.Builder builder;
     Button Buttoncheer;
@@ -41,4 +46,34 @@ public class CheerActivity extends AppCompatActivity {
         startActivity(new Intent(this,  RegiActivity.class)
                 .putExtra("task_id", taskId));
     }
+
+    public void trans1_tap(View v){
+        if(state == 1){
+            imageView.setImageResource(R.drawable.doing);
+            trans2.setVisibility(View.VISIBLE);
+            trans1.setText("中断");
+            trans2.setText("完了");
+        }else if(state == 2){
+            imageView.setImageResource(R.drawable.suspend);
+            trans2.setVisibility(View.VISIBLE);
+            trans1.setText("再開");
+            trans2.setVisibility(View.GONE);
+        }else if(state == 3){
+            imageView.setImageResource(R.drawable.doing);
+            trans2.setVisibility(View.VISIBLE);
+            trans1.setText("中断");
+            trans2.setText("完了");
+        }
+
+
+    }
+
+    public void trans2_tap(View v){
+        if(state == 2){
+            imageView.setImageResource(R.drawable.done);
+            trans1.setVisibility(View.GONE);
+            trans2.setVisibility(View.GONE);
+        }
+    }
+
 }
